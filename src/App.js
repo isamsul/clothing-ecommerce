2471e7ca9823a9449c09215702f8cad7f2912fc6
isamsul <ipaypal1234@gmail.com>
2021-12-23 15:26:28 +0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
