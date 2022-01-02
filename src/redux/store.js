23e411788cd8d6036e97f80eb3eb910b2092370c
isamsul <ipaypal1234@gmail.com>
2022-01-02 13:25:59 +0600

    removed logger form production mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlwares = [logger];
+const middlwares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlwares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlwares));
 
 const persistor = persistStore(store);
 
-export {store ,persistor};
+export { store, persistor };
